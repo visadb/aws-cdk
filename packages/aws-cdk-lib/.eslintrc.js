22e92747441fff89bef41134e18c7b336ff109f6
Nick Lynch <nlynch@amazon.com>
2021-12-17 16:26:08 +0000

    chore(aws-cdk-lib): minify aws-cdk-lib sources

--- packages/aws-cdk-lib/.eslintrc.js
+++ packages/aws-cdk-lib/.eslintrc.js
@@ -1,3 +1,2 @@
-const baseConfig = require('@aws-cdk/cdk-build-tools/config/eslintrc');
-baseConfig.parserOptions.project = __dirname + '/tsconfig.json';
-module.exports = baseConfig;
+const baseConfig=require("@aws-cdk/cdk-build-tools/config/eslintrc");baseConfig.parserOptions.project=__dirname+"/tsconfig.json",module.exports=baseConfig;
+//# sourceMappingURL=.eslintrc.js.map
